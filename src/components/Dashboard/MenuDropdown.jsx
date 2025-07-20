434aa3bc613acfa6e01478c33aba4855644a5664
kkris26 <krisnuartha09@gmail.com>
2025-07-20 08:29:58 +0800

    feat : add homepage to menu dropdown dashboard

--- src/components/Dashboard/MenuDropdown.jsx
+++ src/components/Dashboard/MenuDropdown.jsx
@@ -10,13 +10,17 @@ import { HiMenuAlt4 } from "react-icons/hi";
 import { Link } from "react-router";
 
 const MenuDropdown = ({ tabMenus }) => {
+  const dropDownMenu = [
+    ...tabMenus,
+    { key: "homepage", name: "Homepage", path: "/" },
+  ];
   return (
     <div className="lg:hidden">
       <Dropdown>
         <DropdownTrigger>
           <HiMenuAlt4 className="text-3xl" />
         </DropdownTrigger>
-        <DropdownMenu aria-label="Dynamic Actions" items={tabMenus}>
+        <DropdownMenu aria-label="Dynamic Actions" items={dropDownMenu}>
           {(item) => (
             <DropdownItem key={item.key} className="">
               <Link className="flex w-full " to={item.path}>
