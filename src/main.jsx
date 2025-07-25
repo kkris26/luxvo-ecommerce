503098bbdc3258ad143fe3eeadfed16d946a2cda
kkris26 <krisnuartha09@gmail.com>
2025-07-25 10:19:47 +0800

    chore: setup Redux with store and provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HeroUIProvider>
-      <ToastProvider />
-      <App />
-    </HeroUIProvider>
+    <Provider store={store}>
+      <HeroUIProvider>
+        <ToastProvider />
+        <App />
+      </HeroUIProvider>
+    </Provider>
   </StrictMode>
 );
