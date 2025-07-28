e5800981521a34f58bff917ba9b9757720f986f0
kkris26 <krisnuartha09@gmail.com>
2025-07-28 15:00:52 +0800

    feat : vercel analityc

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store.js";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")).render(
       <HeroUIProvider>
         <ToastProvider />
         <App />
+        <Analytics />
       </HeroUIProvider>
     </Provider>
   </StrictMode>
