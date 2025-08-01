0cd415912d3857286bdb736a37cccb9036120e65
kkris26 <krisnuartha09@gmail.com>
2025-08-01 17:44:07 +0800

    feat : skeleton laoding

--- src/components/Main/ProductGridWrapper.jsx
+++ src/components/Main/ProductGridWrapper.jsx
@@ -7,12 +7,12 @@ const ProductGridWrapper = ({ loading, products, chipLabel, skeleton = 8 }) => {
     <div className="grid grid-cols-4 gap-y-8 gap-x-5 w-full">
       {loading ? (
         [...Array(skeleton)].map((_, i) => <ProductCardSkeleton key={i} />)
-      ) : products.length > 0 ? (
+      ) : products.length ? (
         products.map((p) => (
           <ProductsCard key={p.id} product={p} chipLabel={chipLabel} />
         ))
       ) : (
-        <div className="col-span-4 h-200  flex items-center justify-center flex-col gap-1 place-items-center">
+        <div className="col-span-4 h-50  flex items-center justify-center flex-col gap-1 place-items-center">
           <h3 className="text-xl">No Products Found</h3>
           <p className="text-sm font-light text-gray-500">
             Try adjusting your filters or browse a different category.
--- src/pages/ProductCategory.jsx
+++ src/pages/ProductCategory.jsx
@@ -4,7 +4,7 @@ import { Link, useParams } from "react-router";
 import db from "../db/db";
 import ProductGridWrapper from "../components/Main/ProductGridWrapper";
 import { useSelector } from "react-redux";
-import { Button, Image } from "@heroui/react";
+import { Button, Image, Skeleton } from "@heroui/react";
 
 const ProductCategory = () => {
   const params = useParams();
@@ -37,26 +37,48 @@ const ProductCategory = () => {
   }, []);
   return (
     <>
-      <div className="h-100 relative gap-3 flex flex-col items-center justify-center text-white">
-        <img
-          src={categoryData?.imgUrl}
-          className="absolute h-full  bg-default-500 w-full object-cover -z-1 inset-0 "
-        />
-        <h1 className="text-4xl tracking-wider font-extralight uppercase">
-          {categoryData?.name}
-        </h1>
-        <p className="text-sm font-extralight"> {categoryData?.description}</p>
-        <Link to={"#content"}>
-          <Button
-            variant="bordered"
-            color="white"
-            className="border-1 mt-2"
-            radius="none"
-          >
-            Let's Explore
-          </Button>
-        </Link>
-      </div>
+      {loadingGetCategory ? (
+        <div className="relative w-full h-100 rounded-none overflow-hidden">
+          <div className="absolute inset-0 bg-default-100 w-full h-full">
+         
+          </div>
+          <div className="relative z-10 flex flex-col justify-center items-center h-full px-4 text-center space-y-3">
+            <Skeleton className="w-2/6 rounded-lg">
+              <div className="h-8 w-2/3 bg-default-200 rounded-lg" />
+            </Skeleton>
+            <Skeleton className="w-1/4 rounded-lg">
+              <div className="h-4 w-1/2 bg-default-300 rounded-lg" />
+            </Skeleton>
+            <Skeleton className="w-1/6 rounded-lg">
+              <div className="h-10 w-full bg-default-300 rounded-lg" />
+            </Skeleton>
+          </div>
+        </div>
+      ) : (
+        <div className="h-100 relative gap-3 flex flex-col items-center justify-center text-white">
+          <img
+            src={categoryData?.imgUrl}
+            className="absolute h-full  bg-default-500 w-full object-cover -z-1 inset-0 "
+          />
+          <h1 className="text-4xl tracking-wider font-extralight uppercase">
+            {categoryData?.name}
+          </h1>
+          <p className="text-sm font-extralight">
+            {" "}
+            {categoryData?.description}
+          </p>
+          <Link to={"#content"}>
+            <Button
+              variant="bordered"
+              color="white"
+              className="border-1 mt-2"
+              radius="none"
+            >
+              Let's Explore
+            </Button>
+          </Link>
+        </div>
+      )}
       <div className="py-20" id="content">
         <ProductGridWrapper loading={loading} products={productsByCategory} />
       </div>
