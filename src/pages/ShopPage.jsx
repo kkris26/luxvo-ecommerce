df3655f81dca4128c449caeb2d94b757e13cb940
kkris26 <krisnuartha09@gmail.com>
2025-08-07 23:13:51 +0800

    setup pagination query

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -10,12 +10,19 @@ import {
   Radio,
   RadioGroup,
 } from "@heroui/react";
-import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  where,
+} from "firebase/firestore";
 import db from "../db/db";
 import { useSearchParams } from "react-router";
 
 const ShopPage = () => {
-  //   const { products, loading } = useSelector((state) => state.products);
   const { categories, loadingGetCategory } = useSelector(
     (state) => state.manageCategory
   );
@@ -28,6 +35,11 @@ const ShopPage = () => {
   const [field, setField] = useState("name");
   const [selectKey, setSelectKey] = useState("name-asc");
   const [selectLabel, setSelectLabel] = useState("Name: Z → A");
+  // pagination --
+  const [totalPage, setTotalPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalItems, setTotalItems] = useState(0);
+  // -------------
   const [openFilter, setOpenFilter] = useState(false);
 
   const sortItems = [
@@ -51,6 +63,20 @@ const ShopPage = () => {
       direction: "desc",
     },
   ];
+  const PAGE_LIMIT = 2; // SNAKE_CASE => READ ONLY
+
+  // pagination --
+  function handlePrevPage() {
+    if (currentPage > 1) {
+      setCurrentPage((prevState) => prevState - 1);
+    }
+  }
+  function handleNextPage() {
+    if (currentPage < totalPage) {
+      setCurrentPage((prevState) => prevState + 1);
+    }
+  }
+  // -------------
 
   const getProductsCategories = async () => {
     setLoading(true);
@@ -60,6 +86,27 @@ const ShopPage = () => {
         q = query(q, where("category", "==", filter));
       }
       q = query(q, orderBy(field, sort));
+      // setup pagination
+      const totalItems = (await getDocs(q)).size;
+
+      setTotalItems(totalItems);
+      const currentTotalPage = Math.ceil(totalItems / PAGE_LIMIT);
+      setTotalPage(currentTotalPage);
+      if (currentPage > currentTotalPage) {
+        setCurrentPage(1);
+      }
+      q = query(q, limit(PAGE_LIMIT)); // currentPage = 1 => limit = 2
+      if (currentPage > 1) {
+        q = query(q, limit((currentPage - 1) * PAGE_LIMIT));
+        // currentPage = 2 => limit = 2 => Kacamata Keren => indeks ke-1
+        // currentPage = 3 => limit = 4 => Bayi Rubah => indeks ke-3
+        const documentSnapshots = await getDocs(q);
+        const lastVisible = documentSnapshots.docs.at(-1); // documentSnapshots.docs[documentSnapshots.docs.length - 1]
+
+        console.log(lastVisible.data());
+        q = query(q, limit(PAGE_LIMIT), startAfter(lastVisible));
+      }
+
       const querySnapshot = await getDocs(q);
       const reuslts = querySnapshot.docs.map((doc) => doc.data());
       setFilteredProducts(reuslts);
@@ -72,7 +119,7 @@ const ShopPage = () => {
   useEffect(() => {
     getProductsCategories();
     setSearchParams(`?c=${filter}`);
-  }, [filter, sort, field]);
+  }, [filter, sort, field, currentPage]);
 
   return (
     <div className="flex gap-4">
@@ -95,48 +142,53 @@ const ShopPage = () => {
         </RadioGroup>
       </div>
       <div className="flex flex-col items-end gap-4 w-full">
-        <div
-          onMouseEnter={() => setOpenFilter(true)}
-          onMouseLeave={() => setOpenFilter(false)}
-        >
-          <Dropdown
-            classNames={{
-              base: " -mt-2",
-            }}
-            isOpen={openFilter}
-            onOpenChange={setOpenFilter}
+        <div className="flex  w-full justify-between">
+          <div>
+            <p className="text-sm">Showing {totalItems} products</p>
+          </div>
+          <div
+            onMouseEnter={() => setOpenFilter(true)}
+            onMouseLeave={() => setOpenFilter(false)}
           >
-            <DropdownTrigger>
-              <p className="text-xs" variant="flat">
-                Sort By : {selectLabel}
-              </p>
-            </DropdownTrigger>
-            <DropdownMenu
-              disallowEmptySelection
-              selectedKeys={[selectKey]}
-              selectionMode="single"
-              aria-label="Dynamic Actions"
+            <Dropdown
+              classNames={{
+                base: " -mt-2",
+              }}
+              isOpen={openFilter}
+              onOpenChange={setOpenFilter}
             >
-              {sortItems.map((item) => (
-                <DropdownItem
-                  itemClasses={{
-                    title: "text-xs",
-                  }}
-                  className="text-xs"
-                  key={item.key}
-                  value={item.field}
-                  onClick={() => {
-                    setField(item.field),
-                      setSort(item.direction),
-                      setSelectKey(item.key),
-                      setSelectLabel(item.label);
-                  }}
-                >
-                  {item.label}
-                </DropdownItem>
-              ))}
-            </DropdownMenu>
-          </Dropdown>
+              <DropdownTrigger>
+                <p className="text-xs" variant="flat">
+                  Sort By : {selectLabel}
+                </p>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                selectedKeys={[selectKey]}
+                selectionMode="single"
+                aria-label="Dynamic Actions"
+              >
+                {sortItems.map((item) => (
+                  <DropdownItem
+                    itemClasses={{
+                      title: "text-xs",
+                    }}
+                    className="text-xs"
+                    key={item.key}
+                    value={item.field}
+                    onClick={() => {
+                      setField(item.field),
+                        setSort(item.direction),
+                        setSelectKey(item.key),
+                        setSelectLabel(item.label);
+                    }}
+                  >
+                    {item.label}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+          </div>
         </div>
         <ProductGridWrapper
           grid={4}
@@ -144,6 +196,23 @@ const ShopPage = () => {
           products={filteredProducts}
           skeleton={4}
         />
+        <div className="flex gap-4">
+          <Button
+            isDisabled={currentPage === 1}
+            color="primary"
+            onPress={handlePrevPage}
+          >
+            Prev
+          </Button>
+          <p>{currentPage} / {totalPage}</p>
+          <Button
+            isDisabled={currentPage === totalPage || totalPage === 0}
+            color="primary"
+            onPress={handleNextPage}
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </div>
   );
